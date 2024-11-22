d70a20353e942790c363861931b963b480cca9d9
Stepan <St.shelkovnikov@ya.ru>
2024-11-22 14:38:49 +0300

    Initial commit

--- main.go
+++ main.go
@@ -5,21 +5,27 @@ import (
 	"io"
 )
 
+func compareSlices(a, b []byte) bool {
+	for i := range a {
+		if a[i] != b[i] {
+			return false
+		}
+	}
+	return true
+}
 func Contains(r io.Reader, seq []byte) (bool, error) {
 	buff := make([]byte, 39)
 	n, err := r.Read(buff)
 	if err != nil {
-		return false, errors.New("SUIIIIIIIIIIIII")
+		return false, errors.New("SUIIIIIIII")
 	}
-	if len(seq) < n {
+	if len(seq) > n {
 		return false, errors.New("file is smaller then seq")
 	}
-	//for i := 0; i < n; i++{
-	//if == buff[i:i+3]{
-
-	//}
-	//}
-	return true, nil
-}
-func main() {
+	for i := 0; i < n-len(seq); i++ {
+		if compareSlices(seq, buff[i:i+len(seq)]) {
+			return true, nil
+		}
+	}
+	return false, errors.New("sui")
 }
